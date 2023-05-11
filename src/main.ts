a36a6708a93a2787915b04631a818db42f4f7b7e
Nicolas Knaian <nickknaian@gmail.com>
2023-05-11 01:46:21 +0000

    Update language to not be for sample plugin

--- src/main.ts
+++ src/main.ts
@@ -1,93 +1,36 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-import { SpotifyApi } from './spotify_api';
+import { SpotifyApi, SpotifyAlbum, SpotifyAlbumURL } from './spotify_api';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface AlbumCollectionSettings {
 	spotifyClientId: string;
 	spotifyClientSecret: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: AlbumCollectionSettings = {
 	spotifyClientId: 'your-client-id',
 	spotifyClientSecret: 'your-client-secret'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AlbumCollectionPlugin extends Plugin {
+	settings: AlbumCollectionSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', async (evt: MouseEvent) => {
-			const spotify_api = new SpotifyApi();
-			
-			try {
-				await spotify_api.init(this.settings.spotifyClientId, this.settings.spotifyClientSecret);
-			
-				const album = await spotify_api.albumFromUrl('https://open.spotify.com/album/0fO1KemWL2uCCQmM22iKlj?si=2bb07382c6f54c54');
-				new Notice(`${album?.name} by ${album?.artists[0].name}`);
-			} catch (error) {
-				new Notice(error, 10000);
-			}
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add command to import an album
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'import-album',
+			name: 'Import Album',
+			callback: async () => {
+				new ImportAlbumModal(this.app, this.settings, (albumResult) => {
+					new Notice(`${albumResult?.name} by ${albumResult?.artists[0].name}`);
+				}).open();
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Add settings tab
+		this.addSettingTab(new AlbumCollectionSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -103,14 +46,52 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class ImportAlbumModal extends Modal {
+	settings: AlbumCollectionSettings;
+	spotifyUrl: SpotifyAlbumURL;
+	albumResult: SpotifyAlbum;
+	onSubmit: (albumResult: SpotifyAlbum) => void;
+
+	constructor(app: App, settings: AlbumCollectionSettings, onSubmit: (albumResult: SpotifyAlbum) => void) {
 		super(app);
+		this.settings = settings;
+		this.onSubmit = onSubmit;
 	}
 
-	onOpen() {
+	async onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+
+		// Initialize spotify api token
+		const spotifyApi = new SpotifyApi();
+		try {
+			await spotifyApi.init(this.settings.spotifyClientId, this.settings.spotifyClientSecret);
+		} catch (error) {
+			new Notice(error, 10000);
+			this.close();
+		}
+
+		contentEl.createEl("h2", { text: "Import Spotify Album" });
+
+		new Setting(contentEl)
+			.setName("Album Url")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.spotifyUrl = value as SpotifyAlbumURL;
+			}));
+	
+		new Setting(contentEl)
+			.addButton((btn) => btn
+				.setButtonText("Submit")
+				.setCta()
+				.onClick(async () => {
+					try {
+						this.albumResult = await spotifyApi.albumFromUrl(this.spotifyUrl);
+						this.onSubmit(this.albumResult);
+						this.close();
+					} catch (error) {
+						new Notice(error, 10000);
+					}
+				}));
 	}
 
 	onClose() {
@@ -119,10 +100,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class AlbumCollectionSettingTab extends PluginSettingTab {
+	plugin: AlbumCollectionPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AlbumCollectionPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -135,10 +116,10 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Spotify API Settings'});
 
 		const description = containerEl.createEl('small', {cls: 'settings__description', text: 'You must create your own developer application first. See '});
-		const spotify_api_link = containerEl.createEl('a');
-		spotify_api_link.href = 'https://developer.spotify.com/documentation/web-api/concepts/apps';
-		spotify_api_link.innerText = 'Spotify API guide for creating a developer app'
-		description.append(spotify_api_link);
+		const spotifyApLink = containerEl.createEl('a');
+		spotifyApLink.href = 'https://developer.spotify.com/documentation/web-api/concepts/apps';
+		spotifyApLink.innerText = 'Spotify API guide for creating a developer app'
+		description.append(spotifyApLink);
 
 		new Setting(containerEl)
 			.setName('Spotify API Client ID')
